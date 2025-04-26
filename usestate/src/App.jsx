c5a4a6cf38164edaed3d024e185b1a14ac5cfda3
Danny McCarragher <dannymccarragher@gmail.com>
2025-04-26 10:24:31 -0700

    Setup app.jsx. importing useState and data

--- usestate/src/App.jsx
+++ usestate/src/App.jsx
@@ -1,11 +1,15 @@
 import RestaurantsContainer from "./components/RestaurantsContainer";
 import { restaurants } from "./data/data.js";
 import "./App.css";
+import {data} from './data/data.js';
+import { useState } from "react";
 
 function App() {
+
+  const [restaurantState, setRestaurants] = useState();
   return (
     <div className="App">
-      <RestaurantsContainer />
+      <RestaurantsContainer restaurants={restaurantState} />
     </div>
   );
 }
